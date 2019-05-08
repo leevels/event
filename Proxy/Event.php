4b5fcfdf2a7bc63b67331538eadf62325410a4dc
XiangMin Liu <635750556@qq.com>
2019-05-08 22:23:49 +0800

    refactor(facade): rename facade to proxy

--- Proxy/Event.php
+++ Proxy/Event.php
@@ -18,12 +18,12 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Leevel\Event\Facade;
+namespace Leevel\Event\Proxy;
 
 use Leevel\Di\Container;
 
 /**
- * 门面 event.
+ * 代理 event.
  *
  * @author Xiangmin Liu <635750556@qq.com>
  *
